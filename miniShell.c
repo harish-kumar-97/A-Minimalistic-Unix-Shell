2dd9c7eaa446c5d75e27195dea824c07bb1e31ba
Harish Kumar <harishec031@gmail.com>
2021-02-15 22:00:40 +0530

    implemented cd, echo, pwd, exit functionalities

--- miniShell.c
+++ miniShell.c
@@ -1,3 +1,10 @@
+/****************************************************************
+ * File: miniShell.c
+ * Author: Harish
+ * Description: A minimalistic version of the unix shell
+ *              implemented using C programming language.
+ ***************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,39 +12,72 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#define BUILTIN_COMMANDS 1
+#define BUILTIN_COMMANDS 5
 
 /* environmental variables */
-char PWD[1024];
-char PATH[1024];
+char PWD[1024]; // present working directory
+char PATH[1024]; // path to find the commands
 
 /* built-in command names */
-char *builtin[] = {"cd"};
+char *builtin[] = {"cd", "exit", "help", "pwd", "echo"};
 
-void shell_loop(void);
-char *read_command_line(void);
-char **split_command_line(char *command);
-int shell_execute(char **args);
-int shell_help(char **args);
-int start_process(char **args);
-int shell_cd(char **args);
-
-void shell_loop(void) {
-        // display help at startup
-        int status = shell_help(NULL);
-        char *command_line;
-        char **arguments;
-        status = 1;
-
-        while(status) {
-                printf("minsh> ");
-                command_line = read_command_line();
-                if(strcmp(command_line, "") == 0) {
-                        continue;
-                }
-                arguments = split_command_line(command_line);
-                status = shell_execute(arguments);
+int shell_cd(char **args) {
+        if (args[1] == NULL) {
+                fprintf(stderr, "minsh: atleast one argument is required.\n");
+        } else if (chdir(args[1]) < 0) {
+                perror("minsh");
         }
+        // update present working directory
+        getcwd(PWD, sizeof(PWD));
+        return 1;
+}
+
+int shell_exit(char **args) {
+        return 0;
+}
+
+int shell_pwd(char **args) {
+        printf("%s\n", PWD);
+        return 1;
+}
+
+int shell_echo(char **args) {
+        int i = 1;
+        while(1) {
+                // end of arguments
+                if(args[i] == NULL) {
+                        break;
+                }
+                printf("%s ", args[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+int shell_help(char **args) {
+        printf("\nA mini implementation of the Unix Shell.\n");
+        printf("\nCommands implemented: ");
+        printf("\n\t- help");
+        printf("\n\t- exit");
+        printf("\n\t- cd dir");
+        printf("\n\t- pwd");
+        printf("\n\t- echo [string to echo]");
+        printf("\n\t- clear");
+        printf("\n\t- ls [-all] [dir1 dir2 ...]");
+        printf("\n\t- cp source target (or) cp file1 [file2 ...] dir");
+        printf("\n\t- mv source target (or) mv file1 [file2 ...] dir");
+        printf("\n\t- rm file1 [file2 ...]");
+        printf("\n\t- mkdir dir1 [dir2 ...]");
+        printf("\n\t- rmdir dir1 [dir2 ...]");
+        printf("\n\t- ln [-s] source target");
+        printf("\n\t- cat [file1 file2 ...]");
+        printf("\n\n");
+        printf("\n\n");
+        printf("Other features: ");
+        printf("\n\t* Input, Output and Error Redirection (<, <<, >, >>, 2>, 2>>, respectively");
+        printf("\n\t* Example: ls -i >> outfile 2> errfile [space mandatory around redirection operators!]");
+        printf("\n\n");
+        return 1;
 }
 
 char *read_command_line(void) {
@@ -61,6 +101,14 @@ char *read_command_line(void) {
         return command;
 }
 
+int (* builtin_function[]) (char **) = {
+        &shell_cd, 
+        &shell_exit,
+        &shell_help,
+        &shell_pwd,
+        &shell_echo
+};
+
 char **split_command_line(char *command) {
         int position = 0;
         int no_of_tokens = 64;
@@ -76,6 +124,35 @@ char **split_command_line(char *command) {
         }
         tokens[position] = NULL;
         return tokens;
+}
+
+int start_process(char **args) {
+        int status;
+        pid_t pid, wpid;
+        
+        pid = fork();
+
+        if(pid == 0) { // it's the child process
+                // find the path of the command
+                char cmd_dir[1024];
+                strcpy(cmd_dir, PATH);
+                strcat(cmd_dir, args[0]);
+
+                // execute the required process
+                if(execv(cmd_dir, args) == -1) {
+                        perror("minsh");
+                }
+                exit(EXIT_FAILURE);
+        }
+        else if(pid < 0) {
+                perror("minsh");
+        }
+        else {
+                do {
+                        wpid = waitpid(pid, &status, WUNTRACED);
+                } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+        return 1;
 }
 
 int shell_execute(char **args) {
@@ -209,77 +286,22 @@ int shell_execute(char **args) {
         return ret_status;
 }
 
-int shell_help(char **args) {
-        printf("\nA mini implementation of the Unix Shell.\n");
-        printf("\nCommands implemented: ");
-        printf("\n\t- help");
-        printf("\n\t- exit");
-        printf("\n\t- cd dir");
-        printf("\n\t- pwd");
-        printf("\n\t- echo [string to echo]");
-        printf("\n\t- clear");
-        printf("\n\t- ls [-all] [dir1 dir2 ...]");
-        printf("\n\t- cp source target (or) cp file1 [file2 ...] dir");
-        printf("\n\t- mv source target (or) mv file1 [file2 ...] dir");
-        printf("\n\t- rm file1 [file2 ...]");
-        printf("\n\t- mkdir dir1 [dir2 ...]");
-        printf("\n\t- rmdir dir1 [dir2 ...]");
-        printf("\n\t- ln [-s] source target");
-        printf("\n\t- cat [file1 file2 ...]");
-        printf("\n\n");
-        printf("\n\n");
-        printf("Other features: ");
-        printf("\n\t* Input, Output and Error Redirection (<, <<, >, >>, 2>, 2>>, respectively");
-        printf("\n\t* Example: ls -i >> outfile 2> errfile [space mandatory around redirection operators!]");
-        printf("\n\n");
-        return 1;
-}
+void shell_loop(void) {
+        // display help at startup
+        int status = shell_help(NULL);
+        char *command_line;
+        char **arguments;
+        status = 1;
 
-int start_process(char **args) {
-        int status;
-        pid_t pid, wpid;
-        
-        pid = fork();
-
-        if(pid == 0) { // it's the child process
-                // find the path of the command
-                char cmd_dir[1024];
-                strcpy(cmd_dir, PATH);
-                strcat(cmd_dir, args[0]);
-
-                // execute the required process
-                if(execv(cmd_dir, args) == -1) {
-                        perror("minsh");
+        while(status) {
+                printf("MINI-SHELL: ");
+                command_line = read_command_line();
+                if(strcmp(command_line, "") == 0) {
+                        continue;
                 }
-                exit(EXIT_FAILURE);
+                arguments = split_command_line(command_line);
+                status = shell_execute(arguments);
         }
-        else if(pid < 0) {
-                perror("minsh");
-        }
-        else {
-                do {
-                        wpid = waitpid(pid, &status, WUNTRACED);
-                } while(!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
-        return 1;
-}
-
-int (* builtin_function[]) (char **) = {
-        &shell_cd
-  //      &shell_exit,
-    //    &shell_help,
-     //   &shell_pwd,
-     //   &shell_echo
-};
-
-int shell_cd(char **args) {
-        if(args[1] == NULL) {
-                fprintf(stderr, "minsh: one argument required\n");
-        } else if(chdir(args[1]) < 0) {
-                perror("minsh");
-        }
-        getcwd(PWD, sizeof(PWD));
-        return 1;
 }
 
 /*driver function: main */
